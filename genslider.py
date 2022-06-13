7023eb9844ca2ea57ae704aed4c60ec246a694c3
Franciscus Everdij <f.p.x.everdij@tudelft.nl>
2022-06-14 01:18:13 +0200

    added generation slider

---
+++ genslider.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class GenSlider(tk.Frame):
+    def __init__(self, master):
+        super(GenSlider, self).__init__(master)
+        self.generation = tk.IntVar(0)
+        
+        self.G = tk.Label(self, text='Generation: ')
+        self.G.grid(row=0, column=0, ipadx=4, ipady=5, sticky=tk.E)
+        self.L = tk.Label(self, textvariable=self.generation, width=3)
+        self.L.grid(row=0, column=1, ipadx=4, ipady=5, sticky=tk.W)
+        self.S = tk.Scale(self, variable=self.generation, orient=tk.HORIZONTAL, from_=0, to=10, showvalue=0)
+        self.S.grid(row=0, column=2)
+
--- tkWin.py
+++ tkWin.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from menubar import *
 from cellboard import *
+from genslider import *
 
 class App(tk.Tk):
     def __init__(self, master = None):
@@ -12,7 +13,9 @@ class App(tk.Tk):
         self.title('TkLife')
         self.config(menu = menubar)
         board = CellBoard(self)
-        board.pack()
+        board.grid(row=0, column=0, sticky=tk.NW)
+        slider = GenSlider(self)
+        slider.grid(row=1, column=0, sticky=tk.NW)
         
 if __name__ == "__main__":
     app = App()
